333a38b1e1b0a1ddf47236f38c126ff4b3f777d0
Triden Richards <tridenratu@hotmail.com>
2023-11-21 12:53:10 +1300

    fix to app message

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      it('should return "Gran Tridonia Backend has loaded"', () => {
+          expect(appController.getHello()).toBe('Gran Tridonia Backend has loaded');
     });
   });
 });
